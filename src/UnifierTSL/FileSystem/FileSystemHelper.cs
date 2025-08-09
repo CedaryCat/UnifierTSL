a08ca8a0b167b751000e8b18120cba28c86e2306
CedaryCat <2503439479@qq.com>
2025-08-09 11:36:28 +0800

    fix: replace '.exe' with FileSystemHelper.GetExecutableExtension()

---
+++ src/UnifierTSL/FileSystem/FileSystemHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+
+namespace UnifierTSL.FileSystem
+{
+    public static class FileSystemHelper
+    {
+        public static string GetExecutableExtension() => (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? ".exe" : "");
+    }
+}
--- src/UnifierTSL/Utilities.cs
+++ src/UnifierTSL/Utilities.cs
@@ -1,6 +1,7 @@
 ﻿using Terraria;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
+using System.Runtime.InteropServices;
 
 namespace UnifierTSL
 {
