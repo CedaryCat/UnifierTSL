8ddf7a77bdf34bb556fd84023272bd8196b8f901
CedaryCat <2503439479@qq.com>
2025-08-10 14:05:18 +0800

    fix: change NuGet package folder format to package_name/version & cross-platform support for the console client

--- src/UnifierTSL/FileSystem/FileSystemHelper.cs
+++ src/UnifierTSL/FileSystem/FileSystemHelper.cs
@@ -5,5 +5,12 @@ namespace UnifierTSL.FileSystem
     public static class FileSystemHelper
     {
         public static string GetExecutableExtension() => (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? ".exe" : "");
+        public static string GetDynamicLibraryExtension() => 
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows)? ".dll" :
+            RuntimeInformation.IsOSPlatform(OSPlatform.Linux)? ".so" :
+            RuntimeInformation.IsOSPlatform(OSPlatform.OSX)? ".dylib" :
+            throw new PlatformNotSupportedException("Unsupported OS platform");
+        public static string GetDynamicLibraryPrefix() => RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "" : "lib";
+        public static string GetDynamicLibraryFileName(string name, bool withPrefix = true) => $"{(withPrefix ? GetDynamicLibraryPrefix() : "")}{name}{GetDynamicLibraryExtension()}";
     }
 }
--- src/UnifierTSL/ResolveHelpers.cs
+++ src/UnifierTSL/ResolveHelpers.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.Loader;
+using UnifierTSL.FileSystem;
 using UnifierTSL.Module.Dependencies;
 
 namespace UnifierTSL
@@ -19,23 +20,26 @@ namespace UnifierTSL
             return null;
         }
 
-        internal static nint ResolveNativeDll(Assembly _, string unmanagedDllName) {
+        internal static nint ResolveNativeDll(Assembly _, string unmanagedLibName) {
             var currentRid = RuntimeInformation.RuntimeIdentifier;
             var fallbackRids = RidGraph.Instance.ExpandRuntimeIdentifier(currentRid);
 
             var dir = Directory.GetCurrentDirectory();
 
-            string extension = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? ".dll" :
-                RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? ".so" :
-                RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? ".dylib" :
-                throw new PlatformNotSupportedException("Unsupported OS platform");
+            var fileName1 = FileSystemHelper.GetDynamicLibraryFileName(unmanagedLibName, withPrefix: true);
+            var fileName2 = FileSystemHelper.GetDynamicLibraryFileName(unmanagedLibName, withPrefix: false);
 
             foreach (var rid in fallbackRids) {
-                var currentPath = Path.Combine(dir, "runtimes", rid, "native", unmanagedDllName + extension);
+                var currentPath = Path.Combine(dir, "runtimes", rid, "native", fileName1);
+                if (File.Exists(currentPath)) {
+                    return NativeLibrary.Load(currentPath);
+                }
+                currentPath = Path.Combine(dir, "runtimes", rid, "native", fileName2);
                 if (File.Exists(currentPath)) {
                     return NativeLibrary.Load(currentPath);
                 }
             }
+
             return nint.Zero;
         }
     }
